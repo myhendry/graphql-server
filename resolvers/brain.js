deee1f4e072dea173e9bf8c1a0557929fed316ac
Hendry Lim <myhendry@gmail.com>
2020-03-31 19:44:37 +0800

    added py

--- resolvers/brain.js
+++ resolvers/brain.js
@@ -8,11 +8,12 @@ module.exports = {
       return true;
     },
     testPyMicroService: async () => {
-      const res = await axios.get(
+      const { data } = await axios.get(
         "https://enigmatic-dusk-86110.herokuapp.com/hello"
       );
-      console.log("res: ", res.data);
-      return true;
+      const res = data.msg;
+      console.log("res: ", res);
+      return res;
     }
   }
 };
