86712584569333cd204bd6409142220406c89313
Hendry Lim <myhendry@gmail.com>
2020-04-04 22:20:16 +0800

    updated brain

--- resolvers/brain.js
+++ resolvers/brain.js
@@ -9,31 +9,31 @@ module.exports = {
     },
     testPyHelloService: async () => {
       const { data } = await axios.post(
-        "https://hendry-python-server.herokuapp.com/hello",
+        "https://hl-python-server.herokuapp.com/hello",
         // "http://localhost:5000/hello",
         {
           list: ["you are stupid idiot", "you are cute"],
           name: "Fred",
-          email: "Flintstone@gmail.com"
+          email: "Flintstone@gmail.com",
         }
       );
       const res = data.msg;
       return res;
-    }
+    },
   },
   Mutation: {
     askPySentiment: async (_, { statement }) => {
       const { data } = await axios.post(
-        "https://hendry-python-server.herokuapp.com/brain",
+        "https://hl-python-server.herokuapp.com/brain",
         // "http://localhost:5000/brain",
         {
           list: [statement],
           name: "Fred",
-          email: "Flintstone@gmail.com"
+          email: "Flintstone@gmail.com",
         }
       );
       const res = data.sentiment;
       return res;
-    }
-  }
+    },
+  },
 };
